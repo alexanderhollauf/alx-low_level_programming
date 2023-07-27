b9e34b76057dcde5a1baa4060dd8e718eeeeea8f
Alexander Hollauf <hollaufa@yahoo.com>
2023-07-27 16:52:04 -0700

    0x06. C - More pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strncpy - Copy a string
+ * @dest: Destination buffer
+ * @src: Source string
+ * @n: Number of characters to copy
+ *
+ * Return: Pointer to destination buffer
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int j;
+
+	j = 0;
+
+	while (j < n && src[j] != '\0')
+	{
+		dest[j] = src[j];
+		j++;
+	}
+
+	while (j < n)
+	{
+		dest[j] = '\0';
+		j++;
+	}
+
+	return (dest);
+}
+
---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * leet - Encodes a string into 1337 (leet) speak.
+ * @str: The string to be encoded.
+ *
+ * Return: A pointer to the encoded string.
+ */
+char *leet(char *str)
+{
+	int i;
+	int j;
+	char leet_letters[] = "aAeEoOtTlL";
+	char leet_numbers[] = "4433007711";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; leet_letters[j] != '\0'; j++)
+		{
+			if (str[i] == leet_letters[j])
+				str[i] = leet_numbers[j];
+		}
+	}
+
+	return (str);
+}
+
